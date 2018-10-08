67e4ef842741c940afe234d91af747c08434229e
rafidashab <rafidashabpranta@gmail.com>
2018-10-08 01:26:25 -0700

    Set up android studio

---
+++ app/src/main/java/com/halifax/andriod/schedule/MainActivity.java
@@ -0,0 +1,16 @@
+package com.halifax.andriod.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  //start activity_main.xml
+
+        //Start coding here
+
+    }
+}
